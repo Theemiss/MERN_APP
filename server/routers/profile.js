30a836747648b97dae3bf5354b76bb314e914fbb
Amin Bondi <2212@holbertonschool.com>
2022-08-13 23:30:47 +0100

    Login route

--- server/routers/profile.js
+++ server/routers/profile.js
@@ -33,11 +33,38 @@ router.post("/user/signup", (req, res) => {
       res.json({ err: err });
     });
 });
-router.get("/profile",(req,res) =>{
-    // let token = req.headers['authorization'].slice(7)
-    // const data = JsonWebToken.verify(token=token,SECRET_JWT_CODE)
-    console.log(data)
-    res.json({"msg": "success"})
+router.get("/profile", (req, res) => {
+  // let token = req.headers['authorization'].slice(7)
+  // const data = JsonWebToken.verify(token=token,SECRET_JWT_CODE)
+  console.log(data)
+  res.json({ "msg": "success" })
 })
-//5th step
+
+
+//5th step (Log in)
+router.post("/user/login", (req, res) => {
+  if (!req.body.email || !req.body.password) {
+    res.json({ message: "Missing credentials" });
+  }
+  User.findOne({ email: req.body.email }).then((user) => {
+    if (!user) {
+      res.json({ message: "User does not exist" });
+    }
+    else {
+      if (Bcrypt.compareSync(req.body.password, user.password)) {
+        const Token = JsonWebToken.sign(
+          { id: user._id, email: user.email },
+          SECRET_JWT_CODE
+        );
+        res.json({ token: Token, user: user._id });
+      }
+      else {
+        res.json({ "msg": "wrong password" })
+      }
+    }
+  }).catch((err) => {
+    res.json({ message: err })
+  })
+
+})
 module.exports = router;
